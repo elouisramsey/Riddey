0f8761e7b56238afb0a1093d88e77ea1b8bd081e
Louis <louis.ezzim@crowdforce.io>
2023-02-22 10:32:52 +0100

    minor optimizations

--- src/components/BoardsContainer.tsx
+++ src/components/BoardsContainer.tsx
@@ -1,20 +1,28 @@
 import useCounter from 'hooks/useCounter'
+import { useCallback, useMemo } from 'react'
 import Board from './Board'
 import Button from './Button'
 import CurrentUsage from './CurrentUsage'
 
 const BoardsContainer = () => {
   const { count, increaseCount } = useCounter()
+
+  const boards = useMemo(() => {
+    return [...Array(count)].map((_, i) => (
+      <Board featureNumber={i + 1} key={i} />
+    ))
+  }, [count])
+
+  const handleIncreaseCount = useCallback(() => {
+    increaseCount()
+  }, [increaseCount])
+
   return (
     <section className='boardsContainer'>
-      <section className='boardContainer'>
-        {[...Array(count)].map((_, i) => (
-          <Board featureNumber={i + 1} key={i} />
-        ))}
-      </section>
+      <section className='boardContainer'>{boards}</section>
 
       <CurrentUsage count={count} />
-      <Button onClick={increaseCount} disabled={count === 10}>
+      <Button onClick={handleIncreaseCount} disabled={count === 10}>
         Add New Board
       </Button>
     </section>
